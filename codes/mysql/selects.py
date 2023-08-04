31e6560a8e0a5a4661768230bfb4f90d6ee7a2ac
BongWonHee <leeban1004@gmail.com>
2023-08-04 17:58:56 +0900

    first commit

---
+++ codes/mysql/selects.py
@@ -0,0 +1,15 @@
+# mysql connect package
+# pip  --> conda 
+
+# connect Mysql
+# ip, port, username, password, databass name
+
+# editor - statement
+
+# make select 
+
+#  execute select query
+
+# return resultset and then display
+
+# close
